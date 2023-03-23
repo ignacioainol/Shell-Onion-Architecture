ad9616da66afebdccd8d67946183b318b9fec7a2
Ignacio Ainol <iggnaxios@gmail.com>
2023-03-22 22:59:34 -0300

    modified scaffolding and passing option api as second parameter

--- OnionArchitecture.sh
+++ OnionArchitecture.sh
@@ -1,33 +1,35 @@
+#!/bin/bash
+
 mkdir ~/Documents/$1
 cd ~/Documents/$1
 dotnet new sln
-#Create core Domain
+
+#Create src folder
+mkdir src
+cd src
+#create main folders
+mkdir core infrastructure presentation
+cd core
+# #Create core Domain
 dotnet new classlib -o Domain
 rm Domain/Class1.cs
-dotnet sln $1.sln add Domain
-#Create Application
+mkdir Domain/{Common,Entities}
+# #Create Application
 dotnet new classlib -o Application
 rm Application/Class1.cs
-dotnet sln $1.sln add Application
-#Create Identity
-dotnet new classlib -o Identity
-rm Identity/Class1.cs
-dotnet sln $1.sln add Identity
-#Create Persistance
-dotnet new classlib -o Persistance
-rm Persistance/Class1.cs
-dotnet sln $1.sln add Persistance
-#Create Shared
+mkdir Application/{Behaviours,DTOs,Exceptions,Features,Filters,Interfaces,Mappings,Parameters,Specifications,Wrappers}
+cd Application
+dotnet new --install Yae.Templates
+cd ../../infrastructure
+# #Create Persistence
+dotnet new classlib -o Persistence
+rm Persistence/Class1.cs
+mkdir Persistence/{Configuration,Context,Repository}
+dotnet new class -t Persistence/ServiceExtension
 dotnet new classlib -o Shared
 rm Shared/Class1.cs
-dotnet sln $1.sln add Shared
-#Create folders inside Application
-mkdir -p ~/Documents/$1/Application/{Behaviors,DTOs,Exceptions,Features,Interfaces,Mappings,Parameters,Specifications,Wrappers}
-#Create folders inside Domain
-mkdir -p ~/Documents/$1/Domain/{Common,Entities,Settings}
-#Create folders inside Identity
-mkdir -p ~/Documents/$1/Identity/{Contexts,Models}
-#Create folders inside Persistance
-mkdir -p ~/Documents/$1/Persistance/{Configuration,Contexts,Migrations,Repository}
-#Create folders inside Shared
-mkdir -p ~/Documents/$1/Shared/{Services}
+dotnet new class -t Shared/ServiceExtension
+mkdir Shared/Services
+cd ../..
+pwd
+if [ "$2" == "api" ]; then dotnet new webapi -o src/presentation/API; fi
